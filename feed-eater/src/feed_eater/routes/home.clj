a770620abc999b6182fb084cdad00ab1b77df283
Ramon J. Long III <ramonjlongiii@gmail.com>
2014-05-05 21:54:55 -0400

    Getting data from Redis at /feed

--- feed-eater/src/feed_eater/routes/home.clj
+++ feed-eater/src/feed_eater/routes/home.clj
@@ -12,9 +12,9 @@
 (def listener
   (car/with-new-pubsub-listener (:spec redis-connection)
     {"feeds" (fn f1 [msg] (:p "Channel match: " msg))}
-   (json/write-str msg)
+   ;;(json/write-str msg)
    (car/subscribe  "feeds")
-   (car/get "feeds")
+   ;;(car/get "feeds")
    ))
 
 ;;(defn home-page []
@@ -25,9 +25,14 @@
   (layout/render
    "home.html" {:content listener}))
 
+(defn events-page []
+  (layout/render "home.html" {:content
+  (str "<h1>Feeds:</h1><p>" (wcar redis-connection (car/hgetall "rob") "</p>"))}))
+
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/feed" [] (events-page)))
