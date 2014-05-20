66c6d4e79ff53ce8400322183884587e0ea71b4e
Ramon J. Long III <ramonjlongiii@gmail.com>
2014-05-20 01:25:40 -0400

    Working on feed_eater_erlang and getting ready to replace Redis with Riak.

--- feed_eater_erlang/src/controller/feed_eater_erlang_feed_controller.erl
+++ feed_eater_erlang/src/controller/feed_eater_erlang_feed_controller.erl
@@ -1,5 +1,9 @@
 -module(feed_eater_erlang_feed_controller, [Req]).
 -compile(export_all).
 
+{ok, C} = eredis:start_link().
+{ok, FeedData} = eredis:q(C, ["MGET" | ["rob", "karen", "travis", "emma", "kaitlyn", "jim"]]).
+
 index('GET', []) ->
   {ok, [{content, "<h1 class='h1 dib'>Main Feed</h1>"}]}.
+%%  {ok, [{ok, FeedData}]}.
